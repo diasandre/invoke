49828e14a3e32b3682d467767ba6adc2fc19ce29
andré <andredds12@gmail.com>
2021-04-24 22:02:20 -0300

    fix jitpack build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.32"
+    `maven-publish`
 }
 
 group = "com.github.diasandre.invoke"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
-    maven{
+    maven {
         url = uri("https://jitpack.io")
     }
 }
@@ -18,6 +19,21 @@ dependencies {
     implementation("io.arrow-kt:arrow-core:0.13.1")
 }
 
-tasks.withType<KotlinCompile>() {
+tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+}
+
+java {
+    withSourcesJar()
 }
