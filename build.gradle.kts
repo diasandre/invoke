a2c81e00d3b262eae5280e0e6c06b7807e238ed0
andré <andredds12@gmail.com>
2021-04-24 22:56:31 -0300

    update folder + increase version to 0.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.diasandre.invoke"
-version = "0.1.3"
+version = "0.1.4"
 
 repositories {
     mavenCentral()
@@ -44,7 +44,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.github.diasandre"
             artifactId = "invoke"
-            version = "0.1.3"
+            version = "0.1.4"
 
             from(components["kotlin"])
         }
--- src/main/kotlin/com/github/diasandre/invoke.kt
+++ src/main/kotlin/com/github/diasandre/invoke.kt
@@ -1,3 +1,5 @@
+package com.github.diasandre
+
 import arrow.core.partially1
 
 operator fun <P1, P2, R> ((P1, P2) -> R).invoke(p1: P1): (P2) -> R = partially1(p1)
